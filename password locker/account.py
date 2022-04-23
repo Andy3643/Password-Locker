158fba5887b4fc75265b3cfcdc35dfac6e42468a
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-04-23 10:27:24 +0300

    feat:add a functions that creats a new account of the user.

--- password locker/account.py
+++ password locker/account.py
@@ -1,5 +1,9 @@
 class account:
+    accounts_list = [] #hold a list of all accounts created.
+    
     def __init__(self,username,password):
         self. username = username
         self.password = password
-        pass
+        
+    def add_account(self):
+        account.accounts_list.append(self) #adds the new account details to the accounts list
